e0238d7946e40109e67e5697fb46a2e3f3f6b81b
“gerald <fitzgeraldkachi@gmail.com>
2023-05-25 10:01:48 +0100

    data for the first batch of faq new fresh data

--- src/pages/home/Faq.tsx
+++ src/pages/home/Faq.tsx
@@ -7,6 +7,40 @@ import Subscribe from "../../components/subscribe/Subscribe"
 import RightArrow from '../../assets/svg/RightArrow';
 import LeftArrow from '../../assets/svg/LeftArrow';
 
+const faqArr2 = [
+  {
+    header: 'How can I schedule a consultation or property viewing?',
+    content: `To schedule a consultation or property viewing, you may call us at 08164482813 or email us at ayotundebally@gmail.com. Our team of professionals would be delighted to assist you in finding the perfect property match.`
+  },
+  {
+    header: 'Where can I find information about your current and past real estate projects?',
+    content: `Please visit the "Projects" tab on our website to explore our diverse portfolio of completed and ongoing real estate developments, highlighting the features and amenities that set us apart.`
+  },
+  {
+    header: 'Is investing in property in Nigeria a safe and profitable option?',
+    content: `Yes, investing in real estate, particularly in thriving urban areas like Lagos, has proven to be a secure and profitable avenue for investors. Dembally Limited focuses on offering properties with high potential for capital appreciation and rental income, ensuring a rewarding investment experience.`
+  },
+  {
+    header: 'What legal documentation is necessary for foreign investors looking to invest in Nigerian property?',
+    content: `Foreign investors require a Certificate of Capital Importation (CCI) and must acquire suitable legal advice to navigate the property registration process. We recommend that foreign investors seek the help of reliable legal and real estate professionals, such as our team at Dembally Limited, to ensure a smooth and hassle-free property acquisition experience.`
+  },
+  {
+    header: 'How do I go about investing in a property if I am living abroad?',
+    content: `At Dembally Limited, we can guide you through the entire property acquisition process from afar, ensuring you make informed decisions and receive full assistance in handling legal procedures and documentation.`
+  },
+  {
+    header: 'What kind of returns can I expect from investing in a Dembally Limited property?',
+    content: `While specific returns vary based on factors such as location and property type, our properties are designed to yield high capital appreciation and rental income for investors, making them ideal choices for long-term investment strategies.`
+  },
+  {
+    header: 'Are there any tax implications for property investors in Nigeria?',
+    content: ` Property investors in Nigeria must pay various taxes, such as Land Use Charge, Capital Gains Tax, and Stamp Duty. It is essential to consult with your tax advisor or a real estate professional like Dembally Limited to better understand your tax obligations and benefits.`
+  },
+  // {
+  //   header: '',
+  //   content: ``
+  // },
+]
 const faqArr = [
   {
     header: 'What types of properties does Dembally Limited offer?',
@@ -31,14 +65,6 @@ const faqArr = [
   {
     header: 'What measures do you take to ensure energy efficiency in your properties?',
     content: `We incorporate state-of-the-art energy-saving technologies and design practices in our properties, resulting in reduced utility costs and a minimal carbon footprint.`
-  },
-  {
-    header: 'How can I schedule a consultation or property viewing?',
-    content: `To schedule a consultation or property viewing, you may call us at 08164482813 or email us at ayotundebally@gmail.com. Our team of professionals would be delighted to assist you in finding the perfect property match.`
-  },
-  {
-    header: 'Where can I find information about your current and past real estate projects?',
-    content: `Please visit the "Projects" tab on our website to explore our diverse portfolio of completed and ongoing real estate developments, highlighting the features and amenities that set us apart.`
   },
 ]
 
@@ -89,21 +115,21 @@ const Faq = () => {
           </div>
         </div>
 
-        <div className="object-cover w-full h-full sm:h-screen bg-no-repeat bg-cover bg-center mx-auto bg-[url('/faqsecond.svg')] bg-black bg-opacity-100">
+        <div className="object-cover w-full h-full sm:h-max bg-no-repeat bg-cover bg-center mx-auto bg-[url('/faqsecond.svg')] bg-black bg-opacity-100">
           <div className="absolute h-full w-full bg-black opacity-70 bg-opacity-90"></div>
 
-          <div className="pt-20 pb-32">
+          <div className="py-20">
             <Carousel arrows {...settings}>
               <div className="flex items-center justify-center">
                 {faqArr.map((item, index) => (
-                  <Accordion key={`faq_${index}`} heading={<h1 className="font-semibold my-4 text-lg text-[#FFEEC8]">{item.header}</h1>}>
+                  <Accordion key={`faq_${index}`} heading={<h1 className="font-semibold my-4 text-base text-[#FFEEC8]">{item.header}</h1>}>
                     <p className="pb-4 text-[#FFEEC8]">{item?.content}</p>
                   </Accordion>
                 ))}
               </div>
               <div>
-                {faqArr.map((item, index) => (
-                  <Accordion key={`faq_${index}`} heading={<h1 className="font-semibold my-4 text-lg text-[#FFEEC8]">{item.header}</h1>}>
+                {faqArr2.map((item, index) => (
+                  <Accordion key={`faq_${index}`} heading={<h1 className="font-semibold my-4 text-base text-[#FFEEC8]">{item.header}</h1>}>
                     <p className="pb-4 text-[#FFEEC8]">{item?.content}</p>
                   </Accordion>
                 ))}
