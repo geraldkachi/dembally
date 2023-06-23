5a37a9515dbe771056dbdf6e4086234f7aa1f6c9
“gerald <fitzgeraldkachi@gmail.com>
2023-06-23 21:36:20 +0100

    for nav side bar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,43 +1,83 @@
 import { Modal } from 'antd'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Logonav from '../../assets/svg/Logonav'
+import { ClickOutside } from '../modal/ClickOutside'
 
 interface Props {
     scrollToSection?: () => void | undefined,
 }
 const Navbar = ({ scrollToSection }: Props) => {
+    const [nav, setNav] = useState<boolean>(true);
     const [stateNew, setStateNew] = useState<boolean>(false)
     const navigate = useNavigate()
+
+    useEffect(() => {
+        if (!nav) document.body.style.overflowY = 'hidden'
+        return () => {
+            document.body.style.overflowY = 'unset'
+        }
+    }, [nav])
+
+    const NavIcon = () => <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M21.5 8.14394H3.5C3.09 8.14394 2.75 7.80394 2.75 7.39394C2.75 6.98394 3.09 6.64394 3.5 6.64394H21.5C21.91 6.64394 22.25 6.98394 22.25 7.39394C22.25 7.80394 21.91 8.14394 21.5 8.14394Z" fill="#ffffff" />
+        <path d="M21.5 13.1439H3.5C3.09 13.1439 2.75 12.8039 2.75 12.3939C2.75 11.9839 3.09 11.6439 3.5 11.6439H21.5C21.91 11.6439 22.25 11.9839 22.25 12.3939C22.25 12.8039 21.91 13.1439 21.5 13.1439Z" fill="#ffffff" />
+        <path d="M21.5 18.1439H3.5C3.09 18.1439 2.75 17.8039 2.75 17.3939C2.75 16.9839 3.09 16.6439 3.5 16.6439H21.5C21.91 16.6439 22.25 16.9839 22.25 17.3939C22.25 17.8039 21.91 18.1439 21.5 18.1439Z" fill="#ffffff" />
+    </svg>
 
 
     return (
         // <div className=' sm:fixed top-0 w-full z-50 border-b-2 border-white'>
-            <div className="">
+        <div className="">
             <div className="bg-transparent  max-w-7xl  flex items-center justify-between mx-auto p-4">
                 <Link to="/" className="flex items-center">
                     <Logonav />
                 </Link>
 
 
-                <div className="justify-between items-center sm:hidden flex text-[#8A8986] font-semibold">
+                {/* <div className="justify-between items-center sm:hidden flex text-[#8A8986] font-semibold">
                     <Link to="/" className={`${window.location.pathname === '/' && 'hidden text-white'} mx-3 sm:mx-4 text-[#d1c9b7]`}>Home</Link>
                     <Link to="/about" className={`${window.location.pathname === '/about' && 'hidden text-white'} mx-3 sm:mx-4 text-[#d1c9b7]`}>About</Link>
                     <Link to="/faq" className={`${window.location.pathname === '/faq' && 'hidden text-white'} mx-3 sm:mx-4 text-[#d1c9b7]`}>Faq</Link>
-                </div>
+                </div> */}
                 <div className="justify-between items-center hidden sm:flex text-[#8A8986] font-semibold">
                     <Link to="/" className={`${window.location.pathname === '/' && ' text-white border-b-4 border-[#BC994B]'} mx-3 sm:mx-4 text-[#d1c9b7]`}>Home</Link>
                     <Link to="/about" className={`${window.location.pathname === '/about' && ' text-white border-b-4 border-[#BC994B]'} mx-3 sm:mx-4 text-[#d1c9b7]`}>About</Link>
                     <Link to="/faq" className={`${window.location.pathname === '/faq' && ' text-white border-b-4 border-[#BC994B]'} mx-3 sm:mx-4 text-[#d1c9b7]`}>Faq</Link>
                 </div>
 
-                <div className="sm:flex md:order-2">
+                <div className="sm:flex md:order-2 lg:block hidden">
                     <button type="button" onClick={() => navigate('/contact')} className={`${window.location.pathname === '/contact' && 'hidden'} whitespace-nowrap flex items-center border border-[#F6CD72]  text-white bg-transparent focus:outline-none  font-medium rounded-md text-sm px-5 sm:px-10 py-3 text-center mr-3 md:mr-0`}>
                         Contact Us
                     </button>
 
                 </div>
+
+                <div onClick={() => setNav(prev => !prev)} className='block sm:hidden text-black cursor-pointer'>
+
+                    {nav && <NavIcon />}
+
+                </div>
             </div>
+
+            {/* <ClickOutside onclickoutside={() => setNav(prev => !prev)} className={!nav ? 'fixed left-0 top-0 h-full w-full border-r border-r-gray-300 bg-[#e9eddb] ease-in-out duration-500 shadow-lg z-[9999px]' : 'ease-in-out duration-500 fixed left-[-100%]'}> */}
+            <div
+                // onClick={() => setNav(prev => !prev)}
+                className={!nav ? 'fixed left-0 top-0 h-full w-full border-r border-r-gray-300 bg-[#000] bg-opacity-90 ease-in-out duration-500 shadow-lg z-[19999px]' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+                <div className="flex flex-col justify-evenly items-center py-10 my-auto h-full mx-auto px-5 text-[#8A8986] font-semibold">
+
+                    {/* <div className="justify-between items-center flex-col flex "> */}
+                        <Link to="/" className={`${window.location.pathname === '/' && ' text-white border-b-4 border-[#BC994B]'} mx-3 sm:mx-4 text-[#d1c9b7]`}>Home</Link>
+                        <Link to="/about" className={`${window.location.pathname === '/about' && ' text-white border-b-4 border-[#BC994B]'} mx-3 sm:mx-4 text-[#d1c9b7]`}>About</Link>
+                        <Link to="/faq" className={`${window.location.pathname === '/faq' && ' text-white border-b-4 border-[#BC994B]'} mx-3 sm:mx-4 text-[#d1c9b7]`}>Faq</Link>
+                    {/* </div> */}
+
+                    <button type="button" onClick={() => navigate('/contact')} className={`${window.location.pathname === '/contact' && 'hidden'} w-full whitespace-nowrap flex flex-col items-center border border-[#F6CD72]  bg-[#F6CD72] text-amber-900 focus:outline-none  font-medium rounded-md text-sm px-5 sm:px-10 py-3 text-center md:mr-0 z-[9999px]`}>
+                        Contact Us
+                    </button>
+                </div>
+            </div>
+
 
 
             <div className="overflow-hidden">
