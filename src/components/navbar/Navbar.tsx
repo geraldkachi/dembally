33b23fef7915aae8990a8e4ddf14c5923e1820eb
“gerald <fitzgeraldkachi@gmail.com>
2023-06-24 00:50:47 +0100

    latest

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { Modal } from 'antd'
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Logonav from '../../assets/svg/Logonav'
-import { ClickOutside } from '../modal/ClickOutside'
+// import { ClickOutside } from '../modal/ClickOutside'
 
 import CloseIcon from './close.svg'
 
